faa1c87b5c9d967a38a7c0a251d4410bee8fc185
yujiguy <marcin.szymczyk@c4it.com.pl>
2023-12-07 08:57:46 +0100

    first commit

---
+++ App1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App1
+{
+    internal class Program
+    {
+        public static object Command { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string word = "c4it";
+            Console.WriteLine(word);
+            Console.ReadLine();
+
+        }
+    }
+}
